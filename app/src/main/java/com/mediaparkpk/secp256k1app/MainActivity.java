bfbf1d1c90934400b12858ecd3c3e6e908488009
zaryab <zaryabshakir99@gmail.com>
2020-04-22 13:27:25 +0500

    Updated

--- app/src/main/java/com/mediaparkpk/secp256k1app/MainActivity.java
+++ app/src/main/java/com/mediaparkpk/secp256k1app/MainActivity.java
@@ -3,7 +3,9 @@ package com.mediaparkpk.secp256k1app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
+import com.mediaparkpk.secp256k1android.Secp256K1;
 import com.mediaparkpk.secp256k1android.Secp256k1Wrapper;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,12 +14,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Secp256k1Wrapper secp256k1Wrapper = new Secp256k1Wrapper();
-        byte[] b = secp256k1Wrapper.stringToBytes("aa9ce7ec21a5655d5b54ac57fa2f0b37d0606967679eef32eff16cc84be8fa9c");
-        byte[] pb = secp256k1Wrapper.createPublicKey(b);
-        String s = secp256k1Wrapper.bytesToHex(pb, pb.length);
-        assert(s == "");
-//        secp256k1Wrapper.publicKey();
 
     }
 }
